7c4dde93aa5344b2f784de746c0fa384801c2211
neljn <neljn@simplestudios.net>
2022-04-04 23:57:31 -0700

    Initial project setup. Following Proffesional CMAKE book

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//
+//
+//
+#include <iostream>
+#include "imgui.h"
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
+#include <SDL.h>
+
+int main(){
+    std::cout << "Hello Gui Template" << std::endl;
+    return 0;
+}
